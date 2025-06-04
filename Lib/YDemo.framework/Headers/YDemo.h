feea34b1f4c215d09042ea1bd90c3490758aa3e4
Monkey's Dad <hu_1987@126.com>
2025-06-04 15:48:54 +0800

    add framework

---
+++ Lib/YDemo.framework/Headers/YDemo.h
@@ -0,0 +1,18 @@
+//
+//  YDemo.h
+//  YDemo
+//
+//  Created by 胡晓辉 on 6/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YDemo.
+FOUNDATION_EXPORT double YDemoVersionNumber;
+
+//! Project version string for YDemo.
+FOUNDATION_EXPORT const unsigned char YDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YDemo/PublicHeader.h>
+#import <YDemo/YSource.h>
+
---
+++ Lib/YDemo.framework/Headers/YSource.h
@@ -0,0 +1,16 @@
+//
+//  YSource.h
+//  YDemo
+//
+//  Created by 胡晓辉 on 6/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YSource : NSObject
++ (void)log:(NSString *)str;
+@end
+
+NS_ASSUME_NONNULL_END
